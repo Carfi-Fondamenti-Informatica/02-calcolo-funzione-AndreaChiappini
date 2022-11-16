dadf76238ee0dd02ab98cad785d01b213fe5c249
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-11-16 18:22:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ int main() {
    int a,b,x,y,z;
    cin >> a >> b >> x >> y;
    if (x<0 and y>0) {
-       z= a*x-b*y;
-       cout << z << endl;
+      z= a*x-b*y;
    }
    else if(x>=0 and y<=0){
-        z= a*x*x-b*y;
-        cout << z << endl;
-    }else {
-      z= a*x+b*y*y;
-      cout << z << endl;
+      z= a*x*x-b*y;
    }
+   else {
+      z=a*x+b*y*y;
+   }
+cout << z << endl;
+   
    return 0;
 }
